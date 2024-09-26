399411ccc6450af9e5888eb242de60f6a49062e0
CheDmitrii <dima.chebykin2000@mail.ru>
2024-09-26 21:11:48 +0300

    create task controller and DTOs

---
+++ backend_service/src/main/java/art/com/backend_service/DTO/SprintDTO.java
@@ -0,0 +1,18 @@
+package art.com.backend_service.DTO;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+public class SprintDTO {
+    private UUID id;
+    private String name;
+    private String description;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private UUID projectId;
+    private List<StoryDTO> stories;
+}
---
+++ backend_service/src/main/java/art/com/backend_service/DTO/UserDTO.java
@@ -0,0 +1,15 @@
+package art.com.backend_service.DTO;
+
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+public class UserDTO {
+    private UUID id;
+    private String username;
+    private String first_name;
+    private String last_name;
+    private String email;
+    private String git_nick;
+}
