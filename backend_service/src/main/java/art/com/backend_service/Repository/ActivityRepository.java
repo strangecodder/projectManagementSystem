91c561d0f269743ca80ae4b950b6ccd3c44eda88
CheDmitrii <dima.chebykin2000@mail.ru>
2024-09-27 20:00:19 +0300

    fix repository and change dockerfiles

--- backend_service/src/main/java/art/com/backend_service/Repository/ActivityRepository.java
+++ backend_service/src/main/java/art/com/backend_service/Repository/ActivityRepository.java
@@ -4,8 +4,6 @@ import art.com.backend_service.Entity.Activity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
 @Repository
 public interface ActivityRepository extends JpaRepository<Activity, Long> {
 }
--- backend_service/src/main/java/art/com/backend_service/Repository/CalendarRepository.java
+++ backend_service/src/main/java/art/com/backend_service/Repository/CalendarRepository.java
@@ -2,11 +2,8 @@ package art.com.backend_service.Repository;
 
 import art.com.backend_service.Entity.Calendar;
 import art.com.backend_service.Entity.CalendarId;
-import art.com.backend_service.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.UUID;
 
 @Repository
 public interface CalendarRepository extends JpaRepository<Calendar, CalendarId> {
