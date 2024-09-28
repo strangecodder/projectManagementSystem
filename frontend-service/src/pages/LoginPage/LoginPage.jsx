d386e5cd32a4c0f26ff5b16fe01dacd361374ec0
ArtHouse17 <102299192+ArtHouse17@users.noreply.github.com>
2024-09-28 14:51:13 +0300

    -Added LoginPage

---
+++ frontend-service/src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,20 @@
+import './ProfilePage.css'
+import {Login} from "../../components/Login/Login.jsx";
+
+function LoginPage() {
+
+    return (
+        <>
+            <div className="projects-page">
+                <div className="projects-header">
+                    <Login/>
+                </div>
+                <div className="projects-list">
+                </div>
+            </div>
+        </>
+    )
+
+}
+
+export default LoginPage
--- frontend-service/src/pages/ProfilePage/ProfilePage.jsx
+++ frontend-service/src/pages/ProfilePage/ProfilePage.jsx
@@ -1,20 +1,32 @@
-import './ProfilePage.css'
-import {Login} from "../../components/Login/Login.jsx";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function ProfilePage() {
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+        axios.get('/api/personal-cabinet')
+            .then(response => {
+                setData(response.data);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }, []);
 
     return (
-        <>
-        <div className="projects-page">
-            <div className="projects-header">
-                <Login/>
-            </div>
-            <div className="projects-list">
-            </div>
+        <div>
+            <h2>Личный кабинет</h2>
+            <ul>
+                <li>id: {data.id}</li>
+                <li>Имя: {data.firstName}</li>
+                <li>Фамилия: {data.lastName}</li>
+                <li>Id отдела: {data.departmentId}</li>
+                <li>Электронная почта: {data.email}</li>
+                <li>Имя гита: {data.gitName}</li>
+            </ul>
         </div>
-        </>
-    )
-    
+    );
 }
 
-export default ProfilePage
+export default ProfilePage;
