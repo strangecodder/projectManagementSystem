4410f9f88558156fb250ada32d3f1eedd958480d
mrskeletone <mr_skeletone_1000@mail.ru>
2024-09-28 16:01:27 +0300

    fix: исправлен user

--- backend_service/src/main/java/art/com/backend_service/Entity/UserInfo.java
+++ backend_service/src/main/java/art/com/backend_service/Entity/UserInfo.java
@@ -14,7 +14,7 @@ import java.util.UUID;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class User implements Serializable {
+public class UserInfo implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
