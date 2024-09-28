d17f708c77090d4ad17d99205ed13516dd6844af
ArtHouse17 <102299192+ArtHouse17@users.noreply.github.com>
2024-09-28 16:47:35 +0300

    -Fix EventRepository.java & ProjectRepository.java

--- backend_service/src/main/java/art/com/backend_service/Repository/EventRepository.java
+++ backend_service/src/main/java/art/com/backend_service/Repository/EventRepository.java
@@ -11,26 +11,26 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.sql.Timestamp;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event,Long> {
 
+    @Query("select e from Event e where e.id_event = :id")
     Event findEventById_event(@Param(value = "id") UUID id);
 
-    @Query("select e.name,e.start_time,e.end_time,e.address from Event e join Calendar c on e.id_event = c.event_id where c.user_id =: id")
+    @Query("SELECT e FROM Event e JOIN Calendar c WHERE c.user_id = :id")
     List<Event> getAllByUserId(@Param(value = "id") int id);
 
-    @Query("delete from Event where id_event =: id")
-    void deleteEventById_event(@Param(value = "id")UUID id);
+    @Query("delete from Event where id_event =:id")
+    void deleteEventById_event(@Param(value = "id") UUID id);
 
     @Modifying
     @Transactional
-    @Query("update Event e set e.name =: name, e.start_time =: start_time, e.end_time =:end_time, e.address =:address where e.id_event =:id")
-    void updateEvent(@RequestParam(value = "name")String name,
-                     @RequestParam(value = "start_time")Timestamp start_time,
-                     @RequestParam(value = "end_time") Timestamp end_time,
-                     @RequestParam(value = "address")String address,
-                     @RequestParam(value = "id")UUID id);
+    @Query("update Event e set e.name = :name, e.start_time = :start_time, e.end_time = :end_time, e.address = :address where e.id_event = :id")
+    void updateEvent(@Param(value = "name")String name,
+                     @Param(value = "start_time") Timestamp start_time,
+                     @Param(value = "end_time") Timestamp end_time,
+                     @Param(value = "address")String address,
+                     @Param(value = "id")UUID id);
 }
--- backend_service/src/main/java/art/com/backend_service/Repository/ProjectRepository.java
+++ backend_service/src/main/java/art/com/backend_service/Repository/ProjectRepository.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ProjectRepository extends JpaRepository<Project,Long> {
     @Query("update Project p set p.description =:description  where p.id_project = :id")
-void updateProjectByDescription(@Param("description") String description,@Param("id")Long id);
-    @Query("update Project p set p.name =:name  where p.id_project = :id")
+    void updateProjectByDescription(@Param("description") String description,@Param("id")Long id);
+    @Query("update Project p set p.name =:description  where p.id_project = :id")
     void updateProjectByName(@Param("description") String name,@Param("id")Long id);
 }
